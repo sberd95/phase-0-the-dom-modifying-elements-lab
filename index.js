47f7265749c490e64c705fcd3fa55ba0c8ce6e34
Sergio Berdeja <sergioberdeja@gmail.com>
2022-04-20 21:18:21 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+document.getElementById('main').remove()
+
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.innerHTML = 'Sergio is the champion'
+
+document.body.append(newHeader)
